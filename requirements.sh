99a961ae83463d8d9d445e0ab8e45c9c6acf8598
arsenii <arsyhiy32@gmail.com>
2024-12-12 11:37:54 +0300

    Feat: requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -7,8 +7,11 @@ sudo
 #sudo apt update
 
 sudo python3 --version  || sudo apt install python3
+sudo apt install python3-venv # as i understand you can't check the version so it will just install 
 sudo htop --version || sudo apt install htop
 sudo neofetch --version || sudo apt install neofetch
 sudo firefox --version || sudo apt install firefox
 sudo gcc --version || sudo apt install build-essential
+sudo clang --version || sudo apt install clang
 sudo git --version || sudo apt install git
+sudo nodejs --version || sudo apt install nodejs
