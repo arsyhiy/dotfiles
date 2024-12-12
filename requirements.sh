3b47bc4cafaa80818173fff1886b7c4d11ab3d3e
arsenii <arsyhiy32@gmail.com>
2024-12-12 12:31:30 +0300

    feat: add docker section

--- requirements.sh
+++ requirements.sh
@@ -37,3 +37,5 @@ nvim --version || curl -LO https://github.com/neovim/neovim/releases/latest/down
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 
+# docker section
+sudo docker --version || sudo apt install apt-transport-https ca-certificates curl software-properties-common -y | curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -  | sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | apt-cache policy docker-ce | sudo apt install docker-ce -y | sudo systemctl status docker
