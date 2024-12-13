3edad4ba9b5a4a478b1231ec61a816fadbe850bf
arsenii <arsyhiy32@gmail.com>
2024-12-13 19:34:30 +0300

    my stupid ass doesn't see not important enter space

--- .config/nvim/lua/core/options.lua
+++ .config/nvim/lua/core/options.lua
@@ -1,5 +1,4 @@
 vim.cmd("let g:netrw_liststyle = 3")
-
 local opt = vim.opt
 
 -- numbers that is left from code
@@ -11,13 +10,11 @@ opt.tabstop = 4 -- 2 spaces for tabs (prettier default)
 opt.shiftwidth = 4 -- 2 spaces for indent width
 opt.expandtab = true -- expand tab to spaces
 opt.autoindent = true -- copy indent from current line when starting new one 
-
 opt.wrap = false
 
 -- search settings
 opt.ignorecase = true -- ignore case when searching
 opt.smartcase = true -- if you include mixed case in your search, assumes you want case-sensitive
-
 opt.cursorline = true
 
 -- turn on termguicolors for tokyonight colorscheme to work
