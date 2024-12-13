f1fe3005961992fdd1f520e21535cd935dcf1a5b
arsenii <arsyhiy32@gmail.com>
2024-12-13 19:13:32 +0300

    refactor: unexpected continue of the previous commit

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,8 +1,27 @@
-require("mainDirectory.lazy") -- lazy package manager
 
--- plugins derictory
-require("plugins.telescope")
+
+--Plugins
+require("plugins.alpha")
+require("plugins.autopairs")
+require("plugins.cmp")
 require("plugins.colorscheme")
+require("plugins.dressing")
+require("plugins.indent-blankline")
+require("plugins.lsp")
+require("plugins.lspKeycaps")
+require("plugins.lualine")
+require("plugins.mason")
+require("plugins.miniIcons")
+require("plugins.neotree")
+require("plugins.noice")
+require("plugins.snacks")
+require("plugins.telescope")
+require("plugins.todo-comments")
+require("plugins.treesitter")
+require("plugins.trouble")
+require("plugins.ts-comments")
+require("plugins.whichkey")
 
-require("mainDirectory")
-require("mainDirectory.options")
+
+--Core
+require("core.init")
