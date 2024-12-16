a1c20b124afa8bcf92372ad1e611ceb716356e34
arsenii <arsyhiy32@gmail.com>
2024-12-16 16:58:26 +0300

    delete nvim directory

--- .config/nvim/lua/core/init.lua
+++
@@ -1,4 +0,0 @@
-require("core.lazy")
-require("core.options")
-require("core.remap")
-
--- .config/nvim/lua/core/remap.lua
+++
@@ -1,7 +0,0 @@
-vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>d", vim.cmd.Ex) --Ex command
-
-
--- telescope
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
