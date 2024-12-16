d039e9452c3354c68bf2e888a1e8dd3a5452fe83
arsenii <arsyhiy32@gmail.com>
2024-12-16 17:30:53 +0300

    update CopyMove.sh

--- utils/CopyMove.sh
+++ utils/CopyMove.sh
@@ -2,7 +2,7 @@ printf "\n"
 printf "=======================================================================\n"
 printf "moving neovim.\n"
 printf "=======================================================================\n"
-cp -rv nvim/init.lua ~/config/nvim/
+cp -rv nvim/init.lua ~/.config/nvim/
 cp -rv nvim/lua ~/.config/nvim
 
 printf "=======================================================================\n"
