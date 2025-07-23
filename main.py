af8ea7ebd94a5090f098881010df83f8924648a6
arsyhiy <arsyhiy32@gmail.com>
2025-07-23 18:04:14 +0300

    adding installation from package.txt

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
 import os
 import subprocess
 
-
 def main():
     if os.name == "posix": # linux
         # print and sleep 5 "installing goods for linux"
-        try:
-            subprocess.run(["src/linux/./main.sh"], shell=True, check=True)
-        except subprocess.CalledProcessError as err:
-            print(err)  # need  a more informatic error message
+        install_package()
     elif os.name == "nt": # windows
         # print and sleep 5 "installing goods for windows"
         try:
@@ -21,7 +17,16 @@ def main():
     return subprocess.CompletedProcess
 
 def install_package():
-    pass
+    with open("src/linux/package.txt", "r") as f:
+        packages = [line.strip() for  line in f  if line.strip()]
+        f.close()
+
+    for package in packages:
+        try:
+            subprocess.run(["sudo", "apt", "install", "-y", package], check=True)
+            print(f" Установлен: {package}")
+        except subprocess.CalledProcessError as e:
+            print(f" Ошибка при установке {package}: {e}")
 
 
 
