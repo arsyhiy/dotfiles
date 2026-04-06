d788ee6323b7eac3bb803da3de4941eb8401425e
arsyhiy <arsyhiy32@gmail.com>
2026-04-06 21:05:03 +0300

    add sway waybar

--- cloning_configs.sh
+++ cloning_configs.sh
@@ -9,3 +9,9 @@ git clone https://github.com/arsyhiy/nvim.git ~/.config/nvim
 # ghostty
 git clone https://github.com/arsyhiy/ghostty.git ~/.config/ghostty
 
+# sway
+git clone git@github.com:arsyhiy/sway.git ~/.config/sway/
+
+# waybar
+git clone git@github.com:arsyhiy/waybar.git ~/.config/waybar/
+
