95dae8ba0d4ea29ec16b0c025d4e79e7467a9abf
arsyhiy <arsyhiy32@gmail.com>
2026-04-18 13:30:16 +0300

    update .zshrc and other.sh

--- other.sh
+++ other.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/bash
 
+
+
 sudo apt install -y zsh
+# ohmyzsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# zsh autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions \
+${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 # golang
 # doesn't download the script ~ runs the script directly
