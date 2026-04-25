9dfefe7e1658a3b97155ef93b708e1f5a4d01cb1
arsyhiy <arsyhiy32@gmail.com>
2026-04-25 09:36:45 +0300

    adding wezterm

---
+++ env/.wezterm.lua
@@ -0,0 +1,37 @@
+local wezterm = require 'wezterm'
+
+return {
+  color_scheme = "Builtin gruvbox Dark",
+
+  font = wezterm.font("JetBrains Mono"),
+  font_size = 15.0,
+
+  enable_tab_bar = false,
+  window_decorations = "RESIZE",
+
+  window_background_opacity = 0.80,
+
+  window_padding = {
+    left = 8,
+    right = 8,
+    top = 8,
+    bottom = 8,
+  },
+
+  default_cursor_style = "SteadyBlock",
+
+  scrollback_lines = 3000,
+
+  keys = {
+    {
+      key = "c",
+      mods = "CTRL|SHIFT",
+      action = wezterm.action.CopyTo("Clipboard"),
+    },
+    {
+      key = "v",
+      mods = "CTRL|SHIFT",
+      action = wezterm.action.PasteFrom("Clipboard"),
+    },
+  },
+}
--- other.sh
+++ other.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/bash
 
+sudo apt update
+
 sudo apt install -y zsh
 # ohmyzsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
@@ -14,3 +16,10 @@ bash <(curl -sL https://git.io/go-installer)
 
 # bash formatter
 sudo apt install shfmt
+
+# wezterm
+curl -fsSL https://apt.fury.io/wez/gpg.key | sudo gpg --yes --dearmor -o /usr/share/keyrings/wezterm-fury.gpg
+echo 'deb [signed-by=/usr/share/keyrings/wezterm-fury.gpg] https://apt.fury.io/wez/ * *' | sudo tee /etc/apt/sources.list.d/wezterm.list
+sudo chmod 644 /usr/share/keyrings/wezterm-fury.gpg
+sudo apt install wezterm
+
