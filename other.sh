1a1c845250539312b65502324fb5fd001eff3fe9
Arseniy <arsyhiy32@gmail.com>
2026-06-16 18:03:59 +0300

    adding docker

--- other.sh
+++ other.sh
@@ -12,3 +12,13 @@ ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 # golang
 # doesn't download the script ~ runs the script directly
 bash <(curl -sL https://git.io/go-installer)
+
+# docker
+sudo dnf config-manager addrepo --from-repofile https://download.docker.com/linux/fedora/docker-ce.repo
+
+sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+sudo systemctl enable --now docker
+
+sudo dnf install docker-compose-switch
+sudo usermod -aG docker $USER
