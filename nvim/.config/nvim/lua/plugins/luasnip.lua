96210e71e53242779944809fde696811258b738e
Arseniy <arsyhiy32@gmail.com>
2026-06-21 18:31:37 +0300

    neovim: update luasnip.lua

--- nvim/.config/nvim/lua/plugins/luasnip.lua
+++ nvim/.config/nvim/lua/plugins/luasnip.lua
@@ -4,10 +4,26 @@ return {
     version = "v2.*",
     build = "make install_jsregexp",
     dependencies = {
+      -- 👇 ТВОЙ SNIPPET REPO С GitHub
+      {
+        "arsyhiy/snippets",
+      },
+
       "rafamadriz/friendly-snippets",
     },
+
     config = function()
-      require("luasnip.loaders.from_vscode").lazy_load()
+      local vscode_loader = require("luasnip.loaders.from_vscode")
+
+      -- стандартные снипеты
+      vscode_loader.lazy_load()
+
+      -- 👇 загрузка твоего GitHub snippets plugin
+      vscode_loader.lazy_load({
+        paths = {
+          vim.fn.stdpath("data") .. "/lazy/snippets",
+        },
+      })
     end,
   },
 }
