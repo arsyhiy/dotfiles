925ec125f171c1a82b51268b146239ec5c3c1c87
Arseniy <arsyhiy32@gmail.com>
2026-06-23 07:33:18 +0300

    adding vscode

--- other.sh
+++ other.sh
@@ -9,3 +9,10 @@ sudo systemctl enable --now docker
 
 sudo dnf install docker-compose-switch
 sudo usermod -aG docker $USER
+
+# vscode 
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc &&
+echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
+
+dnf check-update &&
+sudo dnf install code # or code-insiders
