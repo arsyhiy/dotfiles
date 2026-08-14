6db094fce2d5820d0334da528330ac5678fec4fd
Arseniy <arsyhiy32@gmail.com>
2026-08-14 22:11:07 +0300

    now i use ilusia my personal kind of emacs framework!!!

--- .emacs.d/.emacs.d/early-init.el
+++ .emacs.d/.emacs.d/early-init.el
@@ -1,50 +1,146 @@
-;;; early-init.el --- Early initialization -*- lexical-binding: t; -*-
-;; Author: arsyhiy
-;; Package-Requires: ((emacs "30.1"))
+;;; early-init.el --- summary -*- lexical-binding: t; no-byte-compile: t; mode: emacs-lisp; coding:utf-8; fill-column: 80 -*-
 
 ;;; Commentary:
 
-;;; Code:
+;; This is the early-init file. Only for use with emacs 27 or higher. See
+;; https://www.reddit.com/r/emacs/comments/7yns85/emacs_adds_support_for_a_second_read_earlier_init/
+;; and https://lists.gnu.org/archive/html/emacs-devel/2017-10/msg00372.html for
+;; more information.
 
-(setq package-enable-at-startup nil)
+;;;; Speed up startup
+;; Help speed up emacs initialization See
+;; https://blog.d46.us/advanced-emacs-startup/ and
+;; http://tvraman.github.io/emacspeak/blog/emacs-start-speed-up.html and
+;; https://www.reddit.com/r/emacs/comments/3kqt6e/2_easy_little_known_steps_to_speed_up_emacs_start/
+;; This will be set back to normal at the end of the init file
 
-(setq straight-check-for-modifications nil)
-(setq straight-use-package-by-default t)
-
-(defvar bootstrap-version)
-
-(let ((bootstrap-file
-       (expand-file-name
-        "straight/repos/straight.el/bootstrap.el"
-        user-emacs-directory))
-      (bootstrap-version 7))
-  (unless (file-exists-p bootstrap-file)
-    (with-current-buffer
-        (url-retrieve-synchronously
-         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
-         'silent
-         'inhibit-cookies)
-      (goto-char (point-max))
-      (eval-print-last-sexp)))
-  (load bootstrap-file nil 'nomessage))
-
-(straight-use-package 'use-package)
-(require 'use-package)
-
-(setq gc-cons-threshold most-positive-fixnum)
-(setq gc-cons-percentage 1.0)
-
+(defvar lem-file-name-handler-alist file-name-handler-alist)
 (setq file-name-handler-alist nil)
 
-(setq inhibit-startup-screen t
-      inhibit-startup-echo-area-message t)
+;;;; Garbage collection
 
-(setq frame-resize-pixelwise t
-      frame-inhibit-implied-resize t)
+;; Defer garbage collection further back in the startup process. We'll lower
+;; this to a more reasonable number at the end of the init process (i.e. at end of
+;; init.el)
 
-(setq read-process-output-max (* 4 1024 1024))
+(setq gc-cons-threshold most-positive-fixnum)
 
-(setq bidi-display-reordering 'left-to-right
-      bidi-inhibit-bpa t)
+;; Adjust garbage collection thresholds during startup, and thereafter
+;; See http://akrl.sdf.org https://gitlab.com/koral/gcmh
 
+(defmacro k-time (&rest body)
+  "Measure and return the time it takes evaluating BODY."
+  `(let ((time (current-time)))
+     ,@body
+     (float-time (time-since time))))
+
+;; When idle for 15sec run the GC no matter what.
+(defvar k-gc-timer
+  (run-with-idle-timer 15 t
+                       (lambda ()
+                         (let ((inhibit-message t))
+                           (message "Garbage Collector has run for %.06fsec"
+                                    (k-time (garbage-collect)))))))
+
+;;;; Native Comp
+
+;; See https://github.com/jimeh/build-emacs-for-macos#native-comp
+;; https://akrl.sdf.org/gccemacs.html#org335c0de
+;; https://github.com/emacscollective/no-littering/wiki/Setting-gccemacs'-eln-cache
+;; https://debbugs.gnu.org/cgi/bugreport.cgi?bug=53891
+;; https://emacs.stackexchange.com/a/70478/11934
+
+;; See if native-comp is available
+(cond ((not (and (fboundp 'native-comp-available-p)
+                 (native-comp-available-p)))
+       (message "Native complation is *not* available"))
+      ;; Put eln-cache dir in cache directory
+      ;; NOTE the method for setting the eln-cache dir depends on the emacs version
+      ((version< emacs-version "29")
+       (setcar native-comp-eln-load-path
+               (expand-file-name (convert-standard-filename "var/cache/eln-cache/") user-emacs-directory)))
+      (t
+       (startup-redirect-eln-cache (convert-standard-filename (expand-file-name "var/cache/eln-cache/" user-emacs-directory)))))
+
+;; Silence nativecomp warnings popping up
+(customize-set-variable 'native-comp-async-report-warnings-errors nil)
+
+;; Native-comp settings
+(customize-set-variable 'native-comp-speed 2)
+(customize-set-variable 'native-comp-deferred-compilation t)
+
+;; Prefer Newer files
+;; Prefer newer versions of files
+(setq load-prefer-newer t)
+
+;; Byte Compile Warnings
+;; Disable certain byte compiler warnings to cut down on the noise. This is a
+;; personal choice and can be removed if you would like to see any and all byte
+;; compiler warnings.
+(customize-set-variable 'byte-compile-warnings '(not free-vars unresolved noruntime lexical make-local obsolete))
+
+;; Check Errors
+;; Don't produce backtraces when errors occur.
+;; This can be set to `t' interactively when debugging.
+(customize-set-variable 'debug-on-error nil)
+
+;; When-let errors
+;; https://github.com/alphapapa/frame-purpose.el/issues/3
+(eval-and-compile
+  (when (version< emacs-version "26")
+    (with-no-warnings
+      (defalias 'when-let* #'when-let)
+      (function-put #'when-let* 'lisp-indent-function 1)
+      (defalias 'if-let* #'if-let)
+      (function-put #'if-let* 'lisp-indent-function 2))))
+
+;; Variable Binding Depth
+;; This variable controls the number of lisp bindings that can exists at a time.
+;; We should make it fairly large for modern machines.
+;; https://www.reddit.com/r/emacs/comments/9jp9zt/anyone_know_what_variable_binding_depth_exceeds/
+(customize-set-variable 'max-specpdl-size 13000)
+
+;;;; System variables
+      
+(setq user-emacs-directory (expand-file-name user-emacs-directory))
+
+;;;; Customize interface
+
+(setq custom-file (concat user-emacs-directory "persistent/custom.el"))
+(load-file custom-file)
+
+;;;;  core
+
+;; Load path
+;;(add-to-list 'load-path (concat user-emacs-directory "ilusia/core/"))
+;;NOTE: experimental!!!
+;;; Ilusia
+
+(defvar ilusia-directory
+  (expand-file-name "~/ilusia/"))
+
+(dolist (directory '("core" "layers" "modules" "extensions"))
+  (add-to-list 'load-path
+               (expand-file-name directory ilusia-directory)))
+
+; (defconst ilusia-core-components
+;   '("package-manager"
+;     "config-management"))
+;
+; (dolist (component ilusia-core-components)
+;   (require (intern (format "ilusia-core-%s" component))))
+
+; (defconst ilusia-core-components
+;   '("package-manager"
+;     "config-management"))
+;
+; (dolist (component ilusia-core-components)
+;   (require (intern (format "ilusia-%s" component))))
+
+(defconst ilusia-core-components
+  '("package-manager"
+    "config-management"))
+
+(dolist (component ilusia-core-components)
+  (require (intern (format "ilusia-core-%s" component))))
 ;;; early-init.el ends here
