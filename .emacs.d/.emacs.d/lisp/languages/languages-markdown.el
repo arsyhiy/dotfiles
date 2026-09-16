fa49bc9df6728099297070a97c8cde2c6be5eb82
Arseniy <arsyhiy32@gmail.com>
2026-09-16 08:07:51 +0300

    update markdown

--- .emacs.d/.emacs.d/lisp/languages/languages-markdown.el
+++ .emacs.d/.emacs.d/lisp/languages/languages-markdown.el
@@ -9,8 +9,11 @@
 (use-package markdown-mode
   :ensure nil
   :defer t
-  :mode ("README\\.md\\'" . gfm-mode)
-  :init (setq markdown-command "multimarkdown"))
+  :mode ("\\.md\\'" . gfm-mode)
+  (setq markdown-command "multimarkdown"
+        fill-column 80)
+  :hook
+  (gfm-mode . auto-fill-mode))
 
 (provide 'languages-markdown)
 ;;; languages-markdown.el ends here
